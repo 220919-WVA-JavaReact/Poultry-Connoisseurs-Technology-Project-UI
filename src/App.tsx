94f7ee633710c7fc633f57153235b95710779e17
Ian Nelson <ibnelson24@gmail.com>
2022-11-11 18:10:00 -0500

    movie page work

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,6 @@
 import React from "react";
 import { Role } from "./models/role";
-<<<<<<< HEAD
-import logo from './logo.svg';
-import './App.css';
-import Login from './components/Login/Login';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Dashboard from './components/Dashboard/Dashboard';
-import { User } from './models/user';
-import Navbar from './components/Navbar/Navbar';
-import Home from './components/Home/Home';
-import ProfileWelcome from './components/ProfileWelcome/profile-welcome';
-import MoviePage from './components/MoviePage/movie-page';
 
-=======
 import logo from "./logo.svg";
 import "./App.css";
 import Login from "./components/Login/Login";
@@ -23,7 +11,7 @@ import Navbar from "./components/Navbar/Navbar";
 import Home from "./components/Home/Home";
 import ProfileWelcome from "./components/ProfileWelcome/profile-welcome";
 import { Movie } from "./models/movie";
->>>>>>> 82f08fab8e284ae86bb1d966cf8b67b0007d72ee
+import MoviePage from "./components/MoviePage/movie-page";
 
 function App() {
   const [user, setUser] = React.useState<User | undefined>(); //?? need to implement interface somehow. <IUser>
