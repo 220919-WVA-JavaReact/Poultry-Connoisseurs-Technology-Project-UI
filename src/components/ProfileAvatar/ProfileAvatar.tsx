34d3290fe423d6bee58265b7b4f5ea095927a5da
Torie K <victoria.konkiel@yahoo.com>
2022-11-16 10:33:06 -0600

    commiting to merge with dev

---
+++ src/components/ProfileAvatar/ProfileAvatar.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { UserProfile } from '../../models/user';
+
+interface ProfileUserProps{
+    user: UserProfile | undefined;
+}
+
+function ProfileAvatar(props: ProfileUserProps) {
+    
+}
